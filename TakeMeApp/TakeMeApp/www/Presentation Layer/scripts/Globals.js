31824fc3cb5e89419a2441280c89f57bccedab83
AyoubProg <ayoub.aboata@hotmail.com>
2017-08-29 17:17:55 +0300

    add Global.js but you have to change the web serviceURl

---
+++ TakeMeApp/TakeMeApp/www/Presentation Layer/scripts/Globals.js
@@ -0,0 +1,25 @@
+﻿var local = true;
+var WebServiceURL = "http://localhost:63926/UsersWS.asmx"; //the same as above. only with…
+if (!local) {
+    WebServiceURL = "http://ruppinmobile.ac.il.preview26.livedns.co.il/site???/UsersWS.asmx";
+}
+
+
+
+function formatErrorMessage(jqXHR, exception) {
+    if (jqXHR.status === 0) {
+        return ('Not connected.\nPleaseverify your network connection.');
+    } else if (jqXHR.status == 404) {
+        return ('The requested page not found. [404]');
+    } else if (jqXHR.status == 500) {
+        return ('Internal Server Error [500].');
+    } else if (exception === 'parsererror') {
+        return ('Requested JSON parse failed.');
+    } else if (exception === 'timeout') {
+        return ('Time out error.');
+    } else if (exception === 'abort') {
+        return ('Ajax request aborted.');
+    } else {
+        return ('Uncaught Error.\n' + jqXHR.responseText);
+    }
+}
--- TakeMeApp/WebApplication1/App_Code/DAL/LoginDBService.cs
+++ TakeMeApp/WebApplication1/App_Code/DAL/LoginDBService.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
+
 using System.Web;
 using System.Web.Script.Serialization;
 
