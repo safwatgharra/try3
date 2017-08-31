e7ab8a67dabbbe67a805d097857fda68fdf10d6f
AyoubProg <ayoub.aboata@hotmail.com>
2017-08-31 06:15:33 +0300

    add menu

--- TakeMeApp/TakeMeApp/www/Presentation Layer/scripts/homeStudent.js
+++ TakeMeApp/TakeMeApp/www/Presentation Layer/scripts/homeStudent.js
@@ -1,1 +1,13 @@
-﻿
+﻿$(document).ready(function myfunction() {
+
+
+
+    $("#MenuOpen").click(function () {
+        $("#divMenu").css("width", "211px");
+  
+    });
+    $("#MenuClose").click(function () {
+        $("#divMenu").css("width", "0px");
+
+    });
+});
