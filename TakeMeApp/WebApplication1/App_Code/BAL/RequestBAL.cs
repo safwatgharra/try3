eed4b89d82ab64bb71d851224d93280d96fd7d1f
safwatgharra <safwatgharra@gmail.com>
2017-09-04 02:54:20 +0300

    REQUEST BAL + REQUESTDBSERVICE

---
+++ TakeMeApp/WebApplication1/App_Code/BAL/RequestBAL.cs
@@ -0,0 +1,17 @@
+﻿using WebApplication1.App_Code.DAL;
+
+namespace WebApplication1.App_Code.BAL
+{
+    public class RequestBAL
+    {
+        RequestDBService requestdb = new RequestDBService();
+        public RequestBAL()
+        {
+            
+        }
+        public string LoadReguests(int userID)
+        {
+            return requestdb.RequestUser(userID);
+        }
+    }
+}
---
+++ TakeMeApp/WebApplication1/App_Code/DAL/RequestDBService.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Web.Script.Serialization;
+using System.Xml;
+
+namespace WebApplication1.App_Code.DAL
+{
+    public class RequestDBService
+    {
+        string strCon;
+        JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+
+        public RequestDBService()
+        {
+            strCon = DBGlobals.strCon;
+        }
+
+        public object JsonConvert { get; private set; }
+
+        public string RequestUser(int UserID)
+        {
+            SqlConnection con = new SqlConnection(strCon);
+            SqlDataAdapter adptr = new SqlDataAdapter(
+                " SELECT * " +
+                " FROM RequestTB WHERE UserID = '"+UserID+"' and UserPass = '2'", con);
+
+
+            DataSet ds = new DataSet();
+            adptr.Fill(ds, "Requests");
+            DataTable dt = ds.Tables["Requests"];
+
+            //needs the newtonsoft.json from nuget packages!
+            string json = JsonConvert.SerializeObject(dt, Formatting.Indented);
+            return json;
+        }
+    }
+}
